e4d6d231bb1d870cbe671f0811e31adc5435cd78
Łukasz Langa <lukasz@langa.pl>
2009-10-17 13:47:25 +0200

    profile support switch in all management commands

--- src/langacore/kit/django/profile_support.py
+++ src/langacore/kit/django/profile_support.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import os, os.path
+import sys
+from optparse import make_option
+import django.core.management.base
+from django.core.management.base import BaseCommand
+from django.core.management.base import handle_default_options as original_default_options 
 
 local_profile = os.environ.get('DJANGO_SETTINGS_PROFILE', 'local')
 local_settings = '%s/settings-%s.py' % (os.environ.get('DJANGO_SETTINGS_DIR', '.'), local_profile)
@@ -9,8 +14,20 @@ if os.path.exists(local_settings):
     execfile(local_settings)
 elif local_profile != 'local':
     raise ValueError, "%s does not exist." % local_settings
-    
+
 def nothing(*args, **kwargs): pass
     
 import django.core.mail
 #django.core.mail.send_mail = nothing
+
+if not hasattr(BaseCommand, '_langacore_patched'):
+    BaseCommand.option_list = BaseCommand.option_list + (make_option('--profile',
+                        help='Name of a settings module, e.g. "debug", "syncdb", etc. If this isn\'t provided, "local" profile is assumed.'),)
+    BaseCommand._langacore_patched = True
+
+    def handle_default_options(options):
+        if options.profile:
+            os.environ['DJANGO_SETTINGS_PROFILE'] = options.profile
+        original_default_options(options)
+    
+    django.core.management.base.handle_default_options = handle_default_options
