d66e96010129dd7d5cab493185617589e73b9c69
Łukasz Langa <lukasz@langa.pl>
2010-03-08 12:23:17 +0100

    current dir setting fix

--- src/langacore/kit/django/current_dir_support.py
+++ src/langacore/kit/django/current_dir_support.py
@@ -2,13 +2,27 @@
 
 import os.path
 
-SETTINGS_PATH_PREFIX = os.sep.join(os.environ.get('DJANGO_SETTINGS_DIR', CURRENT_DIR), 'settings')
+def _get_settings_path_mode(path):
+    if os.path.isdir(path):
+        return 'nested'
+    elif os.path.exists(path + '.py'):
+        return 'flat'
+    else:
+        return None
 
-if os.path.isdir(SETTINGS_PATH_PREFIX):
-    SETTINGS_PATH_MODE = 'nested'
+
+CURRENT_DIR = os.path.realpath(os.path.dirname(__file__)) + os.path.sep
+_django_settings_dir = os.environ.get('DJANGO_SETTINGS_DIR', CURRENT_DIR)
+
+SETTINGS_PATH_PREFIX = os.path.join(_django_settings_dir, 'settings')
+SETTINGS_PATH_MODE = _get_settings_path_mode(SETTINGS_PATH_PREFIX)
+
+if not SETTINGS_PATH_MODE:
+    # no settings dir/settings.py file in DJANGO_SETTINGS_DIR or the current directory
+    # let's try the parent:
     CURRENT_DIR = os.path.realpath(os.path.dirname(__file__) + os.path.sep + '..') + os.path.sep
-elif os.path.exists(SETTINGS_PATH_PREFIX + '.py'):
-    SETTINGS_PATH_MODE = 'flat'
-    CURRENT_DIR = os.path.realpath(os.path.dirname(__file__)) + os.path.sep
-else:
+    SETTINGS_PATH_PREFIX = os.path.join(_django_settings_dir, '..', 'settings')
+    SETTINGS_PATH_MODE = _get_settings_path_mode(SETTINGS_PATH_PREFIX)
+      
+if not SETTINGS_PATH_MODE:
     raise ValueError, "Django settings structure not recognized."
