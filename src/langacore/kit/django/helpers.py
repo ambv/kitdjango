93a6477495459f4376419f4229a305f08eec0aa4
Łukasz Langa <lukasz@langa.pl>
2010-12-30 20:54:06 +0100

    Choices significantly updated. Now include grouping, elements hold .group and .name. A couple of class methods for getting various values back from a Choices class.

--- src/langacore/kit/django/helpers.py
+++ src/langacore/kit/django/helpers.py
@@ -1,25 +1,35 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# Copyright (C) 2010 Łukasz Langa
+
+"""Various unrelated routines and helper classes."""
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from functools import partial
+import re
+
 from django.conf import settings
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response as _render_to_response
 from django.template import RequestContext
-from django.http import HttpResponseRedirect
-import django.shortcuts
+from django.utils import simplejson
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext
 
 
-def redirect(request, link='/', chain=False):
-    context = RequestContext(request, {})
-    if 'auth__redirect_to' in context:
-        link = context['auth__redirect_to']
+unset = object()
 
-    if chain:
-        if link.rfind('?') != -1:
-            link = link + '&redirect_to=%s' % link[:link.rfind('?')]
-        else:
-            link = link + '?redirect_to=%s' % link 
-
-    return HttpResponseRedirect(link)
+DOTS_REGEX = re.compile(r'\.\s+')
 
 
 def render(request, template_name, context, debug=False, mimetype=None):
+    """Renders the `context` within for a `request` using a template named
+    `template_name` returning the required `mimetype`."""
+
     if hasattr(settings, 'AUTH_PROFILE_MODULE'):
         if 'user_profile' in context:
             raise KeyError, ("langacore.kit.django.helpers.render() doesn't "
@@ -35,64 +45,177 @@ def render(request, template_name, context, debug=False, mimetype=None):
 
     if debug:
         for key in context:
-            print "context['%s'] = %s (type: %s) (repr: %s)" % (key,
-                                                                str(context[key]),
-                                                                type(context[key]),
-                                                                repr(context[key]))
-    return django.shortcuts.render_to_response(template_name,
-                                               RequestContext(request, context),
-                                               mimetype=mimetype)
+            format = (key, str(context[key]), type(context[key]),
+                      repr(context[key]))
+            print("context['{}'] = {} (type: {}) (repr: {})".format(*format))
+    return _render_to_response(template_name,
+                               RequestContext(request, context),
+                               mimetype=mimetype)
 
 
-class Choice(object):
+def render_json(obj):
+    """Dumps the object in the `obj` param to a JSON string and returns it
+    via HTTP using the application/json mimetype."""
+
+    return HttpResponse(simplejson.dumps(obj),
+                        mimetype="application/json")
+
+
+def redirect(request, link='/', override_request=False):
+    """A smarter redirect which takes the redirection target from
+    GET['redirect_to'] or falls back to the URL from the `link` param.
+    If `override_request` is True, `link` is always followed."""
+
+    if not override_request \
+       and request.method == 'GET' \
+       and 'redirect_to' in request.GET:
+        link = request.GET['redirect_to']
+    return HttpResponseRedirect(link)
+
+
+def typical_handler(request, form_class, template, initial={},
+                    initial_kwargs={}):
+    """A handler for a typical form workflow:
+
+    1. Initialize a form object.
+       a.  if there are POSTed data, use them
+       b.  otherwise use `initial` as initial data and `initial_kwargs` as
+           keyword arguments for the form object constructor.
+    2. If the form validates, save it and override the template name with
+       a version using the "_complete.html" suffix.
+    3. If there are errors on the form, prepare an additional `error_summary`
+       string on the form object (for consumption by the template).
+    4. Render the result.
+    """
+    if request.method == 'POST':
+        _kwargs = dict(initial_kwargs)
+        _kwargs['data'] = request.POST
+    else:
+        _kwargs = dict(initial_kwargs)
+        _kwargs['initial'] = initial
+    form = form_class(**_kwargs)
+    if form.is_valid():
+        template = template + '_complete.html'
+        form.save()
+    else:
+        template += '.html'
+        if form.errors:
+            # FIXME: i18n of "Try again" below.
+            error = unicode(" ".join(form.errors.setdefault('__all__',
+                                    [u'Spróbuj ponownie'])))
+            error = DOTS_REGEX.sub('.<br>', error,
+                                   len(DOTS_REGEX.findall(error))-1)
+            form.error_summary = mark_safe(error)
+            del error
+    return render(request, template, locals())
+
+
+def cut(text, length=40, trailing=" (...)"):
+    """Cuts text to a predefined length and appends a trailing ellipsis for
+    longer sources."""
+    if not text:
+        return text
+    if len(text) <= length:
+        trailing = ""
+    return text[:length] + trailing
+
+
+class ChoicesEntry(object):
     global_id = 0
 
-    def __init__(self, description, id=-255):
+    def __init__(self, description, id, name=None):
         self.id = id
-        self.desc = description
+        self.desc = ugettext(description)
         self.global_id = Choice.global_id
-        Choice.global_id += 1
+        self.name = name
+        ChoicesEntry.global_id += 1
+
+
+class ChoiceGroup(ChoicesEntry):
+    def __init__(self, id):
+        super(ChoiceGroup, self).__init__('', id=id)
+        self.choices = []
+
+
+class Choice(ChoicesEntry):
+    def __init__(self, description):
+        super(Choice, self).__init__(description, id=-255)
+        self.group = None
+
+
+def _reverse_impl(cls, id, found=lambda id, k, v: False,
+                    getter=lambda id, k, v: None, fallback=unset):
+    """Unless fallback is set, raises ValueError if name not present."""
+    for k, v in cls.__dict__.items():
+        if isinstance(v, ChoicesEntry) and found(id, k, v):
+            return getter(id, k, v)
+    if fallback is unset:
+        raise ValueError("Nothing found for '{}'.".format(id))
+    else:
+        return fallback
 
 
 class Choices(list):
     Choice = Choice
+    Group = ChoiceGroup
 
     def __init__(self):
         values = []
 
         for k, v in self.__class__.__dict__.items():
-            if type(v) == Choice:
-                 values.append(v)
+            if isinstance(v, ChoicesEntry):
+                v.name = k
+                values.append(v)
 
         if not values:
-            raise ValueError("Choices class declared with no actual choice fields.")
+            raise ValueError("Choices class declared with no actual "
+                             "choice fields.")
 
         values.sort(lambda x, y: x.global_id - y.global_id)
 
         last_choice_id = 0
+        group = None
         for choice in values:
-            if choice.id == -255:
-                last_choice_id += 1
-                choice.id = last_choice_id
-            last_choice_id = choice.id
-            self.append((choice.id, choice.desc))
+            if isinstance(choice, ChoiceGroup):
+                last_choice_id = choice.id
+                group = choice
+            else:
+                if group:
+                    group.choices.append(choice)
+                    choice.group = group
+                if choice.id == -255:
+                    last_choice_id += 1
+                    choice.id = last_choice_id
+                last_choice_id = choice.id
+                self.append((choice.id, choice.desc))
 
+    IDFromName = classmethod(partial(_reverse_impl,
+        found=lambda id, k, v: k == id,
+        getter=lambda id, k, v: v.id))
 
-if __name__ == '__main__':
-    # crude test
-    class Colors(Choices):
-        _ = Choices.Choice
+    DescFromName = classmethod(partial(_reverse_impl,
+        found=lambda id, k, v: k == id,
+        getter=lambda id, k, v: v.desc))
 
-        white = _("White")
-        yellow = _("Yellow")
-        red = _("Red")
-        green = _("Green")
-        black = _("Black")
+    FromName = classmethod(partial(_reverse_impl,
+        found=lambda id, k, v: k == id,
+        getter=lambda id, k, v: v))
 
-    print Colors()
-    print Colors.white.id, Colors.white.desc
+    NameFromID = classmethod(partial(_reverse_impl,
+        found=lambda id, k, v: v.id == id,
+        getter=lambda id, k, v: k))
 
-    class NoChoice(Choices):
-        not_a_choice = "Not a Choice() object"
+    DescFromID = classmethod(partial(_reverse_impl,
+        found=lambda id, k, v: v.id == id,
+        getter=lambda id, k, v: v.desc))
 
-    print NoChoice()
+    FromID = classmethod(partial(_reverse_impl,
+        found=lambda id, k, v: v.id == id,
+        getter=lambda id, k, v: v))
+
+    @staticmethod
+    def ToIDs(func):
+        """Converts a sequence of choices to a sequence of choice IDs."""
+        def wrapper(self):
+            return (elem.id for elem in func(self))
+        return wrapper
---
+++ src/langacore/kit/django/tests/test_helpers.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2010 Łukasz Langa
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Tests for helpers."""
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+def setup():
+    import os
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'langacore.kit.dummy'
+
+def test_dummy():
+    """Just see if the import works as expected."""
+    from langacore.kit.django import helpers
+
+def test_choices_basic():
+    from langacore.kit.django.helpers import Choices
+
+    class Colors(Choices):
+        _ = Choices.Choice
+
+        white = _("White")
+        yellow = _("Yellow")
+        red = _("Red")
+        green = _("Green")
+        black = _("Black")
+
+    assert Colors() == [(1, "White"), (2, "Yellow"), (3, "Red"), (4, "Green"),
+                        (5, "Black")]
+    assert Colors.white.id == 1
+    assert Colors.white.desc == "White"
+    assert Colors.white.name == "white"
+    assert Colors.FromID(1) == Colors.white
+    assert Colors.NameFromID(1) == Colors.white.name
+    assert Colors.DescFromID(1) == Colors.white.desc
+    assert Colors.FromName("white") == Colors.white
+    assert Colors.IDFromName("white") == Colors.white.id
+    assert Colors.DescFromName("white") == Colors.white.desc
+    assert Colors.yellow.id == 2
+    assert Colors.yellow.desc == "Yellow"
+    assert Colors.yellow.name == "yellow"
+    assert Colors.FromID(2) == Colors.yellow
+    assert Colors.NameFromID(2) == Colors.yellow.name
+    assert Colors.DescFromID(2) == Colors.yellow.desc
+    assert Colors.FromName("yellow") == Colors.yellow
+    assert Colors.IDFromName("yellow") == Colors.yellow.id
+    assert Colors.DescFromName("yellow") == Colors.yellow.desc
+    assert Colors.red.id == 3
+    assert Colors.red.desc == "Red"
+    assert Colors.red.name == "red"
+    assert Colors.FromID(3) == Colors.red
+    assert Colors.NameFromID(3) == Colors.red.name
+    assert Colors.DescFromID(3) == Colors.red.desc
+    assert Colors.FromName("red") == Colors.red
+    assert Colors.IDFromName("red") == Colors.red.id
+    assert Colors.DescFromName("red") == Colors.red.desc
+    assert Colors.green.id == 4
+    assert Colors.green.desc == "Green"
+    assert Colors.green.name == "green"
+    assert Colors.FromID(4) == Colors.green
+    assert Colors.NameFromID(4) == Colors.green.name
+    assert Colors.DescFromID(4) == Colors.green.desc
+    assert Colors.FromName("green") == Colors.green
+    assert Colors.IDFromName("green") == Colors.green.id
+    assert Colors.DescFromName("green") == Colors.green.desc
+    assert Colors.black.id == 5
+    assert Colors.black.desc == "Black"
+    assert Colors.black.name == "black"
+    assert Colors.FromID(5) == Colors.black
+    assert Colors.NameFromID(5) == Colors.black.name
+    assert Colors.DescFromID(5) == Colors.black.desc
+    assert Colors.FromName("black") == Colors.black
+    assert Colors.IDFromName("black") == Colors.black.id
+    assert Colors.DescFromName("black") == Colors.black.desc
+
+def test_choices_groups():
+    from langacore.kit.django.helpers import Choices
+
+    class Groupies(Choices):
+        _ = Choices.Choice
+
+        GROUP1 = Choices.Group(0)
+        entry1 = _("entry1")
+        entry2 = _("entry2")
+        entry3 = _("entry3")
+
+        GROUP2 = Choices.Group(10)
+        entry4 = _("entry4")
+        entry5 = _("entry5")
+        entry6 = _("entry6")
+
+        GROUP3 = Choices.Group(20)
+        entry7 = _("entry7")
+        entry8 = _("entry8")
+        entry9 = _("entry9")
+
+    assert Groupies() == [(1, u'entry1'), (2, u'entry2'), (3, u'entry3'),
+                          (11, u'entry4'), (12, u'entry5'), (13, u'entry6'),
+                          (21, u'entry7'), (22, u'entry8'), (23, u'entry9')]
+    assert Groupies.entry1.group == Groupies.GROUP1
+    assert Groupies.entry2.group == Groupies.GROUP1
+    assert Groupies.entry3.group == Groupies.GROUP1
+    assert Groupies.entry4.group == Groupies.GROUP2
+    assert Groupies.entry5.group == Groupies.GROUP2
+    assert Groupies.entry6.group == Groupies.GROUP2
+    assert Groupies.entry7.group == Groupies.GROUP3
+    assert Groupies.entry8.group == Groupies.GROUP3
+    assert Groupies.entry9.group == Groupies.GROUP3
+    assert Groupies.GROUP1.choices == [Groupies.entry1, Groupies.entry2,
+                                       Groupies.entry3]
+    assert Groupies.GROUP2.choices == [Groupies.entry4, Groupies.entry5,
+                                       Groupies.entry6]
+    assert Groupies.GROUP3.choices == [Groupies.entry7, Groupies.entry8,
+                                       Groupies.entry9]
+
+def test_choices_validation():
+    from langacore.kit.django.helpers import Choices
+
+    class NoChoice(Choices):
+        not_a_choice = "Not a Choice() object"
+
+    try:
+        NoChoice()
+        assert False, "ValueError not raised."
+    except ValueError, e:
+        assert str(e) == "Choices class declared with no actual choice fields."
+
+def test_cut():
+    from langacore.kit.django.helpers import cut
+    assert "123456789" == cut("123456789", length=9)
+    assert "123456789 (...)" == cut("123456789ABC", length=9)
+    assert "123456789 ..." == cut("123456789ABC", length=9, trailing=" ...")
+    assert "123456789ABC" == cut("123456789ABC", length=12, trailing=" ...")
+    assert None == cut(None, length=12, trailing=" ...")
+    assert "" == cut("", length=12, trailing=" ...")
+    assert "" == cut("", length=0, trailing=" ...")
+    assert "12345678 (...)" == cut("123456789", length=-1)
