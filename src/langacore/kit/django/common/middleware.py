2f4ab6e3e485ec5d926d9c73ccf57595542bf835
Łukasz Langa <lukasz@langa.pl>
2011-02-07 01:39:31 +0100

    djangobb-like users_online/guests_online cache keys

--- src/langacore/kit/django/common/middleware.py
+++ src/langacore/kit/django/common/middleware.py
@@ -20,9 +20,10 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from django.conf import settings
+from django.core.cache import cache
 from django.utils import translation
 
 class AdminForceLanguageCodeMiddleware:
@@ -43,12 +44,29 @@ class ActivityMiddleware(object):
     """
 
     def process_request(self, request):
-        if not request.user.is_authenticated():
-            return
         now = datetime.now()
         seconds = getattr(settings, 'CURRENTLY_ONLINE_INTERVAL', 120)
-        profile = request.user.get_profile()
-        last_active = profile.last_active
-        if not last_active or (now - last_active).seconds > seconds:
-            profile.last_active = now
-            profile.save()
+        delta = now - timedelta(seconds=seconds)
+        users_online = cache.get('users_online', {})
+        guests_online = cache.get('guests_online', {})
+        if request.user.is_authenticated():
+            users_online[request.user.id] = now
+            profile = request.user.get_profile()
+            last_active = profile.last_active
+            if not last_active or 3 * (now - last_active).seconds > seconds:
+                profile.last_active = now
+                profile.save()
+        else:
+            guest_sid = request.COOKIES.get(settings.SESSION_COOKIE_NAME, '')
+            guests_online[guest_sid] = now
+
+        for user_id in users_online.keys():
+            if users_online[user_id] < delta:
+                del users_online[user_id]
+
+        for guest_id in guests_online.keys():
+            if guests_online[guest_id] < delta:
+                del guests_online[guest_id]
+
+        cache.set('users_online', users_online, 60*60*24)
+        cache.set('guests_online', guests_online, 60*60*24)
