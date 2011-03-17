73d4d99dcd0352aa810fa744270f346746daa8db
Łukasz Langa <lukasz@langa.pl>
2011-03-17 16:13:21 +0100

    get_score_value templatetag introduced

--- src/langacore/kit/django/score/templatetags/lckd_score.py
+++ src/langacore/kit/django/score/templatetags/lckd_score.py
@@ -34,21 +34,20 @@ from langacore.kit.django.score.models import TotalScore
 
 register = template.Library()
 
-class RenderScoreNode(template.Node):
-    """Render the score directly. Some contenttypes related code borrowed from
+
+class ScoreNode(template.Node):
+    """Base score node. Some contenttypes related code taken from
     contrib.comments."""
 
-    template_dir = "score"
-    template_name = "show.html"
-    model = TotalScore
-
-    def __init__(self, ctype=None, object_pk_expr=None, object_expr=None):
+    def __init__(self, ctype=None, object_pk_expr=None, object_expr=None,
+            as_varname=None):
         if ctype is None and object_expr is None:
             raise template.TemplateSyntaxError("Score nodes must be given "
                 "either a literal object or a ctype and object pk.")
         self.ctype = ctype
         self.object_pk_expr = object_pk_expr
         self.object_expr = object_expr
+        self.as_varname = as_varname
 
     def get_target_ctype_pk(self, context):
         if self.object_expr:
@@ -73,18 +72,65 @@ class RenderScoreNode(template.Node):
             raise template.TemplateSyntaxError("%r tag has non-existant "
                 "content-type: '%s.%s'" % (tagname, app, model))
 
-    def __get_query_set(self, context):
-        ctype, object_pk = self.get_target_ctype_pk(context)
-        if not object_pk:
-            return self.model.objects.none()
+    @classmethod
+    def handle_token(cls, parser, token):
+        """Class method to parse get_score* and return a Node."""
+        tokens = token.contents.split()
+        if tokens[1] != 'for':
+            raise template.TemplateSyntaxError("Second argument in %r tag must"
+                " be 'for'" % tokens[0])
 
-        qs = self.model.objects.filter(
-            content_type = ctype,
-            object_id    = smart_unicode(object_pk),
-            # FIXME: sites should probably be present if this is to be really
-            #        reusable
-            #site__id     = settings.SITE_ID,
-        )
+        # {% get_whatever for obj as varname %}
+        if len(tokens) == 5:
+            if tokens[3] != 'as':
+                raise template.TemplateSyntaxError("Third argument in %r must "
+                    "be 'as'" % tokens[0])
+            return cls(
+                object_expr = parser.compile_filter(tokens[2]),
+                as_varname = tokens[4],
+            )
+
+        # {% get_whatever for app.model pk as varname %}
+        elif len(tokens) == 6:
+            if tokens[4] != 'as':
+                raise template.TemplateSyntaxError("Fourth argument in %r must"
+                    " be 'as'" % tokens[0])
+            return cls(
+                ctype = BaseCommentNode.lookup_content_type(tokens[2],
+                    tokens[0]),
+                object_pk_expr = parser.compile_filter(tokens[3]),
+                as_varname = tokens[5]
+            )
+
+        else:
+            raise template.TemplateSyntaxError("%r tag requires 4 or 5 "
+                "arguments" % tokens[0])
+
+    def render(self, context):
+        return ''
+
+    def get_score_value(self, ctype, object_pk):
+        return self.model.objects.get(content_type=ctype,
+            object_id=smart_unicode(object_pk)).value
+
+
+class GetScoreValueNode(ScoreNode):
+    """Inject score value to context."""
+    model = TotalScore
+
+    def render(self, context):
+        ctype, object_pk = self.get_target_ctype_pk(context)
+        if object_pk:
+            context[self.as_varname] = self.get_score_value(ctype, object_pk)
+        return ''
+
+
+class RenderScoreNode(ScoreNode):
+    """Render the score directly."""
+
+    template_dir = "score"
+    template_name = "show.html"
+    model = TotalScore
 
     @classmethod
     def handle_token(cls, parser, token):
@@ -117,8 +163,7 @@ class RenderScoreNode(template.Node):
             ]
             vars = {'ct': ctype, 'object_id': object_pk}
             try:
-                vars['score'] = self.model.objects.get(content_type=ctype,
-                    object_id=smart_unicode(object_pk)).value
+                vars['score'] = self.get_score_value(ctype, object_pk)
             except self.model.DoesNotExist:
                 vars['score'] = 0
             context.push()
@@ -127,6 +172,27 @@ class RenderScoreNode(template.Node):
             return resultstr
         else:
             return ''
+
+@register.tag
+def get_score_value(parser, token):
+    """
+    Gets the score value for the given params and populates the template
+    context with a variable containing that value, whose name is defined by the
+    'as' clause.
+
+    Syntax::
+
+        {% get_score_value for [object] as [varname]  %}
+        {% get_score_value for [app].[model] [object_id] as [varname]  %}
+
+    Example usage::
+
+        {% get_score_value for event as score_value %}
+        {% get_score_value for calendar.event event.id as score_value %}
+        {% get_score_value for calendar.event 17 as score_value %}
+
+    """
+    return GetScoreValueNode.handle_token(parser, token)
 
 @register.tag
 def render_score(parser, token):
