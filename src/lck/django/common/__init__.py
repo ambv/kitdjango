63251f2c8d5a62cfb5d3694f34ff8df8a457032e
Łukasz Langa <lukasz@langa.pl>
2011-07-23 02:11:02 +0200

    nested commit_on_success introduced.

--- src/lck/django/common/__init__.py
+++ src/lck/django/common/__init__.py
@@ -33,6 +33,7 @@ import re
 from zlib import adler32, crc32
 
 from django.conf import settings
+from django.db import transaction
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader, RequestContext
 from django.utils import simplejson
@@ -198,3 +199,25 @@ def remote_addr(request):
             pass
     return result
 
+
+def nested_commit_on_success(func):
+    """Like commit_on_success, but doesn't commit existing transactions.
+
+    This decorator is used to run a function within the scope of a
+    database transaction, committing the transaction on success and
+    rolling it back if an exception occurs.
+
+    Unlike the standard transaction.commit_on_success decorator, this
+    version first checks whether a transaction is already active.  If so
+    then it doesn't perform any commits or rollbacks, leaving that up to
+    whoever is managing the active transaction.
+
+    Taken from: http://djangosnippets.org/snippets/1343/
+    """
+    _commit_on_success = transaction.commit_on_success(func)
+    def _nested_commit_on_success(*args, **kwds):
+        if transaction.is_managed():
+            return func(*args,**kwds)
+        else:
+            return _commit_on_success(*args,**kwds)
+    return transaction.wraps(func)(_nested_commit_on_success)
